4ccfc04cc54ce78d32f8ed88a9bf0e1179fa0950
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-05-01 23:51:03 -0700

    debugger works!!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,14 +42,31 @@
   :ensure t)
 
 
-(use-package nord-theme
-  :ensure t
-  :config (load-theme 'nord))
-
-
 (use-package doom-modeline
   :ensure t
   :config (doom-modeline-mode 1))
+
+
+(use-package nord-theme
+  :ensure t
+  :config
+  (defvar my:theme 'nord)
+  (defvar my:theme-window-loaded nil)
+  (defvar my:theme-terminal-loaded nil)
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions
+		(lambda (frame)
+		  (select-frame frame)
+		  (if (window-system frame)
+		      (unless my:theme-window-loaded
+			(if my:theme-terminal-loaded (enable-theme my:theme) (load-theme my:theme t))
+			(setq my:theme-window-loaded t))
+		      (unless my:theme-terminal-loaded
+			(if my:theme-window-loaded (enable-theme my:theme) (load-theme my:theme t))
+			(setq my:theme-terminal-loaded t)))))
+    (progn
+      (load-theme my:theme t)
+      (if (display-graphic-p) (setq my:theme-window-loaded t) (setq my:theme-terminal-loaded t)))))
 
 
 (use-package evil
@@ -63,7 +80,7 @@
   :init
   (setq company-minimum-prefix-length 1
 	company-idle-delay 0.0
-	company-backends '(company-capf))
+	company-backends '(company-capf company-cmake company-files))
   :config (global-company-mode 1))
 
 
@@ -138,6 +155,14 @@
   :ensure t)
 
 
+(use-package treemacs
+  :ensure t)
+
+
+(use-package treemacs-evil
+  :ensure t)
+
+
 (defun find-user-init-file ()
   (interactive)
   (find-file user-init-file))
@@ -158,7 +183,7 @@
 
 
 (general-define-key
- :states '(normal emacs)
+ :states '(normal visual emacs)
  :keymaps 'override
  :prefix "SPC"
  "." 'find-file
@@ -168,10 +193,33 @@
  "f" 'counsel-projectile-find-file
  "g" 'counsel-projectile-rg
  "i" 'find-user-init-file
+ "t" 'treemacs
  "d" 'recompile
  "D" 'compile
  "m" 'magit
  "s" 'eshell)
+
+
+(general-define-key
+ :states '(visual emacs)
+ :keymaps 'override
+ :prefix "SPC"
+ "c" 'comment-or-uncomment-region)
+
+
+(general-define-key
+ :states '(normal emacs)
+ :keymaps 'override
+ :prefix "SPC"
+ "c" 'comment-line)
+
+
+(general-define-key
+ :states '(normal visual emacs)
+ :keymaps 'override
+ :prefix ","
+ "d" 'dap-hydra
+ "p" 'dap-debug)
 
 
 (general-define-key
@@ -183,7 +231,6 @@
  "C-l" 'evil-window-right)
  
 
-
 (general-define-key
  :states '(normal emacs)
  :keymaps 'override
@@ -194,49 +241,65 @@
  "b" 'evil-jump-backward)
 
 
+(use-package posframe
+  :ensure t)
+
+
+(use-package dap-mode
+  :ensure t
+  :config
+  (dap-mode 1)
+  (dap-ui-mode 1)
+  (dap-tooltip-mode 1)
+  (tooltip-mode 1)
+  (dap-ui-controls-mode 1))
+
+
+(use-package cmake-mode
+  :ensure t)
+
+
+(use-package auto-package-update
+  :ensure t)
+
+
+(defun my-c++-mode-before-save-hook ()
+  (when (eq major-mode 'c++-mode)
+    (lsp-format-buffer)))
+
+
+(add-hook 'before-save-hook #'my-c++-mode-before-save-hook)
+
+
+(dap-register-debug-provider
+ "cppdbg"
+ (lambda (conf)
+   (plist-put conf
+              :dap-server-path
+              '("/Users/adamkowalski/.vscode-insiders/extensions/ms-vscode.cpptools-0.28.0-insiders/debugAdapters/OpenDebugAD7"))
+   conf))
+
+
+(dap-register-debug-template "C++ Run Configuration"
+                             (list :type "cppdbg"
+                                   :cwd "/Users/adamkowalski/code/omega/build/bin"
+                                   :request "launch"
+                                   :program "/Users/adamkowalski/code/omega/build/bin/test_omega"
+				   :externalTerminal :json-false
+				   :MIMode "lldb"
+				   :name "Debug Test"))
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#222222" "#fc618d" "#7bd88f" "#fce566" "#5ad4e6" "#5ad4e6" "#5ad4e6" "#f7f1ff"])
  '(custom-safe-themes
-   '("7f6d4aebcc44c264a64e714c3d9d1e903284305fd7e319e7cb73345a9994f5ef" "7c4cfa4eb784539d6e09ecc118428cd8125d6aa3053d8e8413f31a7293d43169" "d71aabbbd692b54b6263bfe016607f93553ea214bc1435d17de98894a5c3a086" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "24132f7b6699c6e0118d742351b74141bac3c4388937e40db9207554eaae78c9" "8a0c35b74b0407ca465dd8db28c9136d5f539868d4867565ee214ac85ceb0d3a" "58c3313b4811ed8b30239b1d04816f74d438bcb72626d68181f294b115b7220d" "d5f8099d98174116cba9912fe2a0c3196a7cd405d12fa6b9375c55fc510988b5" "f2b56244ecc6f4b952b2bcb1d7e517f1f4272876a8c873b378f5cf68e904bd59" "f9cae16fd084c64bf0a9de797ef9caedc9ff4d463dd0288c30a3f89ecf36ca7e" "e964832f274625fa45810c688bdbe18caa75a5e1c36b0ca5ab88924756e5667f" "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "fa3bdd59ea708164e7821574822ab82a3c51e262d419df941f26d64d015c90ee" "845103fcb9b091b0958171653a4413ccfad35552bc39697d448941bcbe5a660d" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" default))
- '(fci-rule-color "#585858")
- '(jdee-db-active-breakpoint-face-colors (cons "#131313" "#fce566"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#131313" "#7bd88f"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#131313" "#525053"))
- '(modern-c++-font-lock-global-mode t)
- '(objed-cursor-color "#fc618d")
+   '("7f6d4aebcc44c264a64e714c3d9d1e903284305fd7e319e7cb73345a9994f5ef" default))
  '(package-selected-packages
-   '(nord-theme nord evil-magit magit flycheck key-chord modern-cpp-font-lock lsp-ivy lsp-ui lsp-mode counsel-projectile general which-key ivy use-package exec-path-from-shell evil doom-themes doom-modeline dashboard company))
- '(pdf-view-midnight-colors (cons "#f7f1ff" "#222222"))
- '(rustic-ansi-faces
-   ["#222222" "#fc618d" "#7bd88f" "#fce566" "#5ad4e6" "#5ad4e6" "#5ad4e6" "#f7f1ff"])
- '(vc-annotate-background "#222222")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#7bd88f")
-    (cons 40 "#a6dc81")
-    (cons 60 "#d1e073")
-    (cons 80 "#fce566")
-    (cons 100 "#fcc95f")
-    (cons 120 "#fcae59")
-    (cons 140 "#fd9353")
-    (cons 160 "#c6a884")
-    (cons 180 "#90beb5")
-    (cons 200 "#5ad4e6")
-    (cons 220 "#90adc8")
-    (cons 240 "#c687aa")
-    (cons 260 "#fc618d")
-    (cons 280 "#d15c7e")
-    (cons 300 "#a75870")
-    (cons 320 "#7c5461")
-    (cons 340 "#585858")
-    (cons 360 "#585858")))
- '(vc-annotate-very-old-color nil))
-
+   '(treemacs-evil auto-package-update cmake-mode posframe dap-mode spinner evil-magit magit flycheck key-chord modern-cpp-font-lock lsp-ivy lsp-ui lsp-mode which-key general counsel-projectile projectile ivy exec-path-from-shell company evil nord-theme doom-modeline all-the-icons use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

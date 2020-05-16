6ba1cec32aee1165515536c33d2a591598367b58
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-05-16 15:09:05 -0700

    cpp debug test at point

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,6 @@
       ring-bell-function 'ignore
       straight-use-package-by-default t)
 
-
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -16,13 +15,11 @@
 (display-time-mode)
 (show-paren-mode 1)
 
-
 (let ((family "FiraMono Nerd Font")
       (height 160))
   (set-face-attribute 'default nil :family family :height height :weight 'normal :width 'normal)
   (set-face-attribute 'mode-line nil :family family :height height)
   (set-face-attribute 'mode-line-inactive nil :family family :height height))
-
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -37,9 +34,7 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-
 (straight-use-package 'use-package)
-
 
 (use-package evil
   :init
@@ -48,10 +43,8 @@
 	evil-want-keybinding nil)
   :config (evil-mode))
 
-
 (use-package evil-collection
   :config (evil-collection-init))
-
 
 (use-package company
   :init
@@ -60,9 +53,7 @@
 	company-backends '(company-capf company-files))
   :config (global-company-mode 1))
 
-
 (use-package all-the-icons)
-
 
 (use-package doom-themes
   :init
@@ -73,7 +64,6 @@
   (load-theme 'gruvbox-material t)
   (doom-themes-treemacs-config)
   (doom-themes-org-config))
-
 
 (use-package doom-modeline
   :init
@@ -87,13 +77,10 @@
   (display-battery-mode)
   (doom-modeline-mode 1))
 
-
 (use-package exec-path-from-shell
   :config (exec-path-from-shell-initialize))
 
-
 (use-package ivy :config (ivy-mode 1))
-
 
 (use-package projectile
   :init (setq projectile-project-search-path '("~/code/"))
@@ -104,19 +91,15 @@
   :commands (counsel-projectile-find-file
 	     counsel-projectile-switch-project
 	     counsel-projectile-rg))
- 
 
 (use-package general :config (general-override-mode))
 
-
 (use-package which-key :config (which-key-mode))
-
 
 (use-package treemacs
   :init (setq treemacs-lock-width nil
 	      treemacs--width-is-locked nil)
   :commands treemacs)
-
 
 (use-package lsp-mode
   :commands lsp-deferred
@@ -124,44 +107,34 @@
   (setq lsp-prefer-capf t
 	lsp-semantic-highlighting 'immediate))
 
-
 (use-package lsp-ui :commands lsp-ui-mode)
-
 
 (use-package lsp-ivy
   :after ivy
   :commands lsp-ivy-workspace-symbol)
 
-
 (use-package lsp-treemacs :after treemacs)
-
 
 (defun lsp-python-mode-hook ()
   (require 'lsp-python-ms)
   (lsp-deferred))
 
-
 (use-package lsp-python-ms
   :hook (python-mode . lsp-python-mode-hook))
-
 
 (defun lsp-c++-mode-hook ()
   (set (make-local-variable 'compile-command)
        (format "cd %s/build && conan build .." (projectile-project-root)))
   (lsp-deferred))
-  
 
 (defun c++-mode-before-save-hook ()
   (when (eq major-mode 'c++-mode)
     (lsp-format-buffer)))
 
-
 (add-hook 'c++-mode-hook 'lsp-c++-mode-hook)
 (add-hook 'before-save-hook #'c++-mode-before-save-hook)
 
-
 (use-package posframe)
-
 
 (use-package dap-mode
   :straight (dap-mode :type git :host github :repo "emacs-lsp/dap-mode"
@@ -170,66 +143,35 @@
   :init (setq dap-auto-show-output nil)
   :config
   (require 'dap-python)
+  (require 'dap-cppdbg)
   (dap-mode 1)
   (dap-ui-mode 1)
   (dap-tooltip-mode 1)
   (tooltip-mode 1)
   (dap-ui-controls-mode 1))
 
-
-
-(dap-register-debug-provider
- "cppdbg"
- (lambda (conf)
-   (plist-put conf
-              :dap-server-path
-              '("/Users/adamkowalski/.vscode-insiders/extensions/ms-vscode.cpptools-0.28.0-insiders/debugAdapters/OpenDebugAD7"))
-   conf))
-
-
-(dap-register-debug-template
- "C++ Debug Test"
- (list :type "cppdbg"
-       :cwd "/Users/adamkowalski/code/omega/build/bin"
-       :request "launch"
-       :program "/Users/adamkowalski/code/omega/build/bin/test_omega"
-       :externalTerminal :json-false
-       :MIMode "lldb"
-       :name "Debug Test"))
-
-
-
 (use-package key-chord :config (key-chord-mode 1))
-
 
 (use-package flycheck)
 
-
 (use-package magit :commands magit)
 
-
 (use-package evil-magit :after magit)
-
 
 (use-package treemacs-icons-dired
   :after (treemacs all-the-icons)
   :config (treemacs-icons-dired-mode 1))
 
-
 (use-package treemacs-evil :after (treemacs evil))
-
 
 (use-package treemacs-projectile
   :after (treemacs projectile)
   :config (add-hook 'projectile-after-switch-project-hook
 		      'treemacs-display-current-project-exclusively))
 
-
 (use-package avy)
 
-
 (use-package cmake-mode)
-
 
 (use-package dashboard
   :init
@@ -242,56 +184,45 @@
 			  (agenda . 5)))
   :config (dashboard-setup-startup-hook))
 
-
 (defun find-user-init-file ()
   (interactive)
   (find-file user-init-file))
-
 
 (general-define-key :states 'motion ";" 'evil-ex)
 
 (general-define-key :keymaps 'evil-insert-state-map
                     (general-chord "jk") 'evil-normal-state)
 
-
 (defun disable-all-themes ()
   (dolist (i custom-enabled-themes)
     (disable-theme i)))
-
 
 (defun switch-theme ()
   (interactive)
   (disable-all-themes)
   (call-interactively 'load-theme))
 
-
 (defun colorize-compilation-buffer ()
   (let ((inhibit-read-only t))
     (ansi-color-apply-on-region (point-min) (point-max))))
 
-
 (use-package ansi-color
   :config (add-hook 'compilation-filter-hook 'colorize-compilation-buffer))
-
 
 (use-package rainbow-delimiters
   :config (rainbow-delimiters-mode 1))
 
-
 (use-package evil-cleverparens
   :hook ((emacs-lisp-mode . evil-cleverparens-mode)))
 
-
 (use-package adjust-parens
   :straight (adjust-parens :type git :host github :repo "emacs-straight/adjust-parens"))
-
 
 (general-define-key
  :states '(insert normal visual emacs)
  :keymaps 'emacs-lisp-mode-map
  "TAB" 'lisp-indent-adjust-parens
  "<backtab>" 'lisp-dedent-adjust-parens)
-
 
 (defun gitter-irc ()
   (interactive)
@@ -300,7 +231,6 @@
 	   :nick "adam-r-kowalski"
 	   :full-name "Adam Kowalski"
 	   :password (read-string "Enter your password:")))
-
 
 (general-define-key
  :states '(normal visual emacs)
@@ -323,20 +253,17 @@
  "T" 'switch-theme
  "G" 'gitter-irc)
 
-
 (general-define-key
  :states '(visual emacs)
  :keymaps 'override
  :prefix "SPC"
  "c" 'comment-or-uncomment-region)
 
-
 (general-define-key
  :states '(normal emacs)
  :keymaps 'override
  :prefix "SPC"
  "c" 'comment-line)
-
 
 (general-define-key
  :states '(normal visual emacs)
@@ -345,6 +272,11 @@
  "n" 'dap-python--debug-test-at-point
  "f" 'dap-python--debug-test-buffer)
 
+(general-define-key
+ :states '(normal visual emacs)
+ :keymaps 'c++-mode-map
+ :prefix "SPC t"
+ "n" 'dap-cppdbg--debug-test-at-point)
 
 (general-define-key
  :states '(normal visual emacs)
@@ -352,7 +284,6 @@
  :prefix ","
  "d" 'dap-hydra
  "p" 'dap-debug)
-
 
 (general-define-key
  :states '(normal visual insert emacs)
@@ -362,7 +293,6 @@
  "C-k" 'evil-window-up
  "C-l" 'evil-window-right)
  
-
 (general-define-key
  :states '(normal emacs)
  :keymaps 'override
@@ -372,7 +302,6 @@
  "r" 'lsp-ui-peek-find-references
  "s" 'lsp-ivy-global-workspace-symbol
  "b" 'evil-jump-backward)
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

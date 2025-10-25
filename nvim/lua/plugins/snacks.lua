59a4020fc41a464ea06a17b7827c8a473105f4c7
Adam Kowalski <adam.kowalski.work@gmail.com>
2025-10-25 15:13:06 -0700

    embracing snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,8 +3,26 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
+		animate = { enabled = true },
 		bigfile = { enabled = true },
 		picker = { enabled = true },
+		dim = { enabled = true },
+		image = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		lazygit = { enabled = true },
+		zen = {
+			enabled = true,
+			toggles = {
+				dim = true,
+			},
+			win = {
+				backdrop = {
+					transparent = false,
+					blend = 99,
+				},
+			},
+		},
 	},
 	keys = {
 		{
@@ -12,7 +30,35 @@ return {
 			function()
 				require("snacks").picker.files()
 			end,
-			desc = "Find files",
+			desc = "Files",
+		},
+		{
+			"<leader>/",
+			function()
+				require("snacks").picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>b",
+			function()
+				require("snacks").picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>z",
+			function()
+				require("snacks").zen()
+			end,
+			desc = "Zen",
+		},
+		{
+			"<leader>g",
+			function()
+				require("snacks").lazygit()
+			end,
+			desc = "Git",
 		},
 	},
 }

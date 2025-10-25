655f3e67310e212a4072059816f413ee32a0c6dd
Adam Kowalski <adam.kowalski.work@gmail.com>
2025-10-25 15:33:22 -0700

    smart indent and search

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,6 +7,9 @@ vim.opt.termguicolors = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.signcolumn = "yes"
 
@@ -14,3 +17,8 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8

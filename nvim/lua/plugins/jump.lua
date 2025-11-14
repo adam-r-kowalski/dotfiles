0da2384d1197a804534a9d4361c8b36d29c2c763
Adam Kowalski <adam.kowalski.work@gmail.com>
2025-11-13 21:33:22 -0800

    completions

---
+++ nvim/lua/plugins/jump.lua
@@ -0,0 +1,2 @@
+vim.pack.add({ "https://github.com/nvim-mini/mini.jump.git" })
+require("mini.jump").setup()
---
+++ nvim/lua/plugins/snippets.lua
@@ -0,0 +1,2 @@
+vim.pack.add({ "https://github.com/nvim-mini/mini.snippets.git" })
+require("mini.snippets").setup()
